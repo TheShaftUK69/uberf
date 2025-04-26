a737f9e9990305fe6e2835918af0a7556cd70238
TheShaftUK69 <julescovuk@gmail.com>
2025-04-26 23:39:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3006;
 const POOL_CONFIG = {
   host: process.env.DB_HOST || 'uberfnew:us-central1:uberf',
-  user: process.env.DB_USER || '',
+  user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || 'Caitlin69!!!',
   database: process.env.DB_NAME || 'uberf',
   waitForConnections: true,
